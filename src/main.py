228155a6b7fdc16fc1e2f6144c1e2d49153cab72
Emmanuel Yartey <supremeconquerorx@gmail.com>
2026-04-28 12:11:53 +0000

    calculate the total number of comments and average number of comments

--- src/main.py
+++ src/main.py
@@ -1,18 +1,33 @@
 import csv
+header = list()
+data = list()
 
 with open("data/hacker_news.csv", "r", encoding="utf-8") as f:
     reader = csv.reader(f)
     header= next(reader)
     print(", ".join(header))
-
-    data = list()
-    for i,row in enumerate(reader):
+    for row in reader:
         data.append(row)
 
-    show_hn_list = [row for row in data if row[1].startswith("Show HN") ]
-    ask_hn_list = [row for row in data if row[1].startswith("Ask HN") ]
-    print("show_hn_list", len(show_hn_list))
-    print("ask_hn_list", len(ask_hn_list))
+show_hn_posts = [row for row in data if row[1].lower().startswith("show hn") ]
+ask_hn_posts = [row for row in data if row[1].lower().startswith("ask hn") ]
+print("show_hn_list", len(show_hn_posts))
+print("ask_hn_list", len(ask_hn_posts))
+
+ask_comments_num = 0
+
+for row in ask_hn_posts:
+    ask_comments_num += int(row[4])
+
+if len(ask_hn_posts) > 0:
+    avg_ask_comments_num  = ask_comments_num / len(ask_hn_posts)
+else:
+    avg_ask_comments_num = 0
+
+print("ask_comments_num", ask_comments_num)
+print("avg_ask_comments_num", avg_ask_comments_num)
+
+
 
 
 
